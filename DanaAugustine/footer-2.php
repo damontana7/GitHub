c20625b2cbae5b225620daeff921627320194134
damontana-desktop <49513168+damontana7@users.noreply.github.com>
2019-07-31 19:42:23 +1000

    Extra footer widgets

---
+++ DanaAugustine/footer-2.php
@@ -0,0 +1,73 @@
+<?php gelli_footer_top('footer_2') ?>
+<div id="footer_v2" class="footer footer-v2">
+	<div class="container-fluid footer-bottom">
+		<div class="col-md-6 col-lg-12 col-sm-12 col-xs-12">
+			<div class="row">
+				<div class="col-1 col-lg-2 col-md-6 col-sm-6 col-xs-12">
+					<?php if(is_active_sidebar('footerv2-col1')){
+						dynamic_sidebar('footerv2-col1');
+					} ?>   
+				</div>
+				<div class="col-2 col-lg-2 col-md-6 col-sm-6 col-xs-12 footer-info">
+					<?php if(is_active_sidebar('footerv2-col2')){
+						dynamic_sidebar('footerv2-col2');
+					} ?>
+				</div>
+				<div class="col-3 col-lg-2 col-md-6 col-sm-6 col-xs-12">
+					 <?php if(is_active_sidebar('footerv2-col3')){
+						dynamic_sidebar('footerv2-col3');
+					} ?>        
+				</div>
+				<div class="col-4 col-lg-2 col-md-6 col-sm-6 col-xs-12">
+					 <?php if(is_active_sidebar('footerv2-col4')){
+						dynamic_sidebar('footerv2-col4');
+					} ?>
+				</div>
+				<div class="col-4 col-lg-2 col-md-6 col-sm-6 col-xs-12">
+					<aside id="nav_menu-6" class="widget widget_nav_menu"><h4 class="widget-title">Swana</h4><div class="menu-swana-container"><ul id="menu-swana-1" class="menu"><li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/swana/">Swana</a></li>
+						<li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/swana/technological-brilliance/">Technological Brilliance</a></li>
+						<li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/swana/giving-back/">Giving Back</a></li>
+						<li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/swana/the-swana-perfection/">The Swana Perfection</a></li>
+						<li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/swana/understanding-diamonds/">Understanding Diamonds</a></li>		
+						
+</ul></div>
+<h4 class="widget-title">Artemis</h4></aside>
+					<ul id="menu-Artemis-1" class="menu"><li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/artemis/">Artemis</a></li>
+						
+							
+						
+</ul>
+				</div>
+				
+				<div class="col-4 col-lg-2 col-md-6 col-sm-6 col-xs-12">
+					<aside id="nav_menu-6" class="widget widget_nav_menu"><h4 class="widget-title">Contact</h4><div class="menu-swana-container"><ul id="menu-contact-1" class="menu"><li class="https://www.danaaugustineinc.com/contact-us/"><a href="http://timwsuccess.com/demos/dai/contact-us/">Contact</a></li>
+						<li class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-120161"><a href="https://www.danaaugustineinc.com/careers/">Careers</a></li>		
+						
+</ul></div>
+</aside>
+					
+				</div>
+			</div>
+		</div>
+		<!--<div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+			<div class="row">
+				<div class="col-3 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+					 <?php if(is_active_sidebar('footerv2-col3')){
+						dynamic_sidebar('footerv2-col3');
+					} ?>        
+				</div>
+				<div class="col-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+					 <?php if(is_active_sidebar('footerv2-col4')){
+						dynamic_sidebar('footerv2-col4');
+					} ?>
+				</div>
+				<div class="col-4 col-lg-4 col-md-6 col-sm-6 col-xs-12">
+					 <?php if(is_active_sidebar('footerv2-col4')){
+						dynamic_sidebar('footerv2-col4');
+					} ?>
+				</div>
+			</div>
+		</div>-->
+	</div>
+</div><!-- #header_v1-->
+	
